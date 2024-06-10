9fd363e877f686e4a49e76924a857b73d55c7dd7
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-06-10 16:54:26 +0200

    Build: add logical diagram of JS steps

---
+++ js/main.js
@@ -0,0 +1,23 @@
+//Creare un carosello che scorra le immagini caricate
+
+//inserire su JS le immagini che voglio far scorrere
+
+// estrapolare le immagini del mio array
+
+// creare il div di inserimento delle immagini
+
+// inserire le immagini nel mio div
+
+// inserire la classe active del CSS al primo elemento della lista 
+
+// creare e gestire l'evento click
+
+// far spostare la classe active in modo crescente
+
+// creare e gestire l'evento secondo click
+
+// far spostare la classe active in modo decrescente
+
+// stampare in console
+
+// Stampare in pagina
